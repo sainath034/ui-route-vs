99041c06869163a39a2fee14941b3730f593293a
Sainath Balaguru Surender <sainath.revalsys@outlook.com>
2017-07-23 15:37:20 +0530

    Seed Push

---
+++ AngularVS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AngularVS.Global" Language="C#" %>
---
+++ AngularVS/scripts/App.js
@@ -0,0 +1,20 @@
+﻿var myApp = angular.module("myApp", ['ui.router']);
+
+myApp.config(function ($stateProvider, $urlRouterProvider) {
+
+     $urlRouterProvider.when("", "/page1");
+
+     $stateProvider
+        .state("page1", {
+            url: "/page1",
+            templateUrl: "Page-1.html"
+        })
+        .state("page2", {
+            url:"/page2",
+            templateUrl: "Page-2.html"
+        })
+        .state("page3", {
+            url:"/page3",
+            templateUrl: "Page-3.html"
+        });
+});
